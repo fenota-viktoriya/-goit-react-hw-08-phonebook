9540cbcd36826a434dab23fc88e533ce71cbed8c
fenota-viktoriya <Fen9206@gmail.com>
2022-07-29 22:21:07 +0300

    fix:adds basename

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook/">
             <App />
           </BrowserRouter>
         </PersistGate>
@@ -22,4 +22,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </ThemeProvider>
   </React.StrictMode>
 );
-//  basename="/goit-react-hw-08-phonebook/"
